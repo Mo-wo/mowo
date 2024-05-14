fde2cb85d4a8346899530bbd6dba1ea4425d3b63
mowo <toryvick20@gmail.com>
2024-05-15 00:56:29 +0100

    Completed projects section for now

--- src/components/Client/Projects.tsx
+++ src/components/Client/Projects.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import styles from "styles/projects.module.css";
 import "animate.css";
-import Image from "next/image";
 import Link from "next/link";
 import { HeadingText } from 'components/lv2/HeadingText';
 import links from "images/projects/links.png";
@@ -9,7 +8,6 @@ import daniella from "images/projects/daniella.png";
 import medEd from "images/projects/medEd.png";
 import stone from "images/projects/stone.png";
 import truex from "images/projects/truex.png";
-import speedax from "images/projects/speedax.png";
 import iungo from "images/projects/iungo.png";
 
 export const Projects = () => {
@@ -21,12 +19,11 @@ export const Projects = () => {
 
   const projects = [
     {id:1, name: 'Links', thumbnail: links, url:'https://www.linkstheapp.ng/', desc: 'A mobile application that connects individuals with service providers and businesses.', status:'live'},
-    {id:2, name: 'Daniella', thumbnail: daniella, url:'https://daniella.vercel.app/', desc: 'A portfolio website for a product designer', status:'live'},
-    {id:3, name: 'Stone', thumbnail: stone, url:'', desc: 'A portfolio website for a product designer', status:'live'},
-    {id:4, name: 'MedEd Cloud', thumbnail: medEd, url:'https://bcforraji-414038-react.b414038.dev.eastus.az.svc.builder.cafe/', desc: '', status:'live'},
-    {id:5, name: 'TruexGold', thumbnail: truex, url:'', desc: '', status:'live'},
-    // {id:6, name: 'Speedax', thumbnail: speedax, url:'', desc: '', status:''},
-    {id:7, name: 'Iungogram', thumbnail: iungo, url:'', desc: '', status:''},
+    {id:2, name: 'Daniella', thumbnail: daniella, url:'https://daniella.vercel.app/', desc: 'A portfolio website for a product designer.', status:'live'},
+    {id:3, name: 'Stone', thumbnail: stone, url:'', desc: 'A portfolio website for a product designer.', status:'live'},
+    {id:4, name: 'MedEd Cloud', thumbnail: medEd, url:'https://bcforraji-414038-react.b414038.dev.eastus.az.svc.builder.cafe/', desc: 'A healthcare education web application that creates personalized and adaptive learning experiences for medical students and professionals.', status:'live'},
+    {id:5, name: 'TruexGold', thumbnail: truex, url:'', desc: 'A cryptocurrency peer-to-peer web application.', status:''},
+    {id:6, name: 'Iungogram', thumbnail: iungo, url:'', desc: 'A video chat mobile application to connect with favorite celebrities.', status:''},
   ]
 
   return (
@@ -34,7 +31,8 @@ export const Projects = () => {
       <HeadingText heading={'Projects'} />
       <div className={styles.projectsContainer}>
         {projects.map((project:any, index:number) => {
-          const expand = expandedProject === project.id
+          const expand = expandedProject === project.id;
+          const live = project.status === 'live'
           return (
           <div key={index.toString()} style={{ backgroundImage: `url(${project.thumbnail.src})` }} className={expand ? `${styles.projectsWrapper} ${styles.projectsWrapperExpand}` : styles.projectsWrapper}>
             <div className={expand ? `${styles.overlay} ${styles.overlayExpand}` : styles.overlay} onClick={() => handleExpand(project.id)}>
@@ -43,7 +41,7 @@ export const Projects = () => {
                 <h3 className={expand ? styles.nameExpand : styles.name}>{project.name}</h3>
                 <p className={styles.desc}>{project.desc}</p>
                 <Link href={project.url}  passHref legacyBehavior>
-                  <a className={styles.link} target="_blank">{project.status === 'live' ? 'View' : 'View Design System'}
+                  <a style={{width: live ? '80px' : '210px'}} className={styles.link} target="_blank">{project.status === 'live' ? 'View' : 'View Design System'}
                     </a>
                     </Link>
               </div>
